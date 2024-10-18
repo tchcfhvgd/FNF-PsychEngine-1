f9e431f605208d138ef8eb14bbe93501aca43851
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-18 19:52:32 +0800

    Update MusicBeatSubstate.hx

--- source/backend/MusicBeatSubstate.hx
+++ source/backend/MusicBeatSubstate.hx
@@ -1,6 +1,12 @@
 package backend;
 
 import flixel.FlxSubState;
+#if mobile
+import mobile.flixel.FlxVirtualPad;
+import flixel.FlxCamera;
+import flixel.input.actions.FlxActionInput;
+import flixel.util.FlxDestroyUtil;
+#end
 
 class MusicBeatSubstate extends FlxSubState
 {
@@ -25,6 +31,59 @@ class MusicBeatSubstate extends FlxSubState
 	inline function get_controls():Controls
 		return Controls.instance;
 
+	        #if mobile
+		public static var virtualPad:FlxVirtualPad;
+		//var trackedInputsVirtualPad:Array<FlxActionInput> = [];
+
+		public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode)
+		{
+			if (virtualPad != null)
+			removeVirtualPad();
+
+			virtualPad = new FlxVirtualPad(DPad, Action);
+			add(virtualPad);
+                        Controls.checkState = false;
+		        Controls.CheckPress = true;
+			//controls.setVirtualPadUI(virtualPad, DPad, Action);
+			//trackedInputsVirtualPad = controls.trackedInputsUI;
+			//controls.trackedInputsUI = [];
+		}
+
+		public function removeVirtualPad()
+		{
+			if (virtualPad != null)
+			remove(virtualPad);
+		}
+
+		#if mobile
+	        public function noCheckPress() 
+	        {
+		        Controls.CheckPress = false;
+	        }
+	        #end
+	
+	        public function addVirtualPadCamera(DefaultDrawTarget:Bool = true)
+		{
+			if (virtualPad != null)
+			{
+				var camControls:FlxCamera = new FlxCamera();
+				FlxG.cameras.add(camControls, DefaultDrawTarget);
+				camControls.bgColor.alpha = 0;
+				virtualPad.cameras = [camControls];
+			}
+		}
+		#end
+
+		override function destroy()
+		{
+			super.destroy();
+			
+			#if mobile
+			if (virtualPad != null)
+			virtualPad = FlxDestroyUtil.destroy(virtualPad);
+			#end
+		}
+	
 	override function update(elapsed:Float)
 	{
 		//everyStep();
