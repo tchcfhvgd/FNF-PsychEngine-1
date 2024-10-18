9b49badcb874c0bb1aa1ba93979e7cf70b9a859a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-18 20:45:11 +0800

    Add files via upload

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -9,7 +9,7 @@ import lime.system.Clipboard;
 import flixel.util.FlxGradient;
 import objects.StrumNote;
 import objects.Note;
-
+import flixel.addons.transition.FlxTransitionableState;
 import shaders.RGBPalette;
 import shaders.RGBPalette.RGBShaderReference;
 
@@ -48,6 +48,8 @@ class NotesSubState extends MusicBeatSubstate
 	var tipTxt:FlxText;
 
 	public function new() {
+                controls.isInSubstate = true;
+
 		super();
 		
 		#if DISCORD_ALLOWED
@@ -88,8 +90,19 @@ class NotesSubState extends MusicBeatSubstate
 		var bg:FlxSprite = new FlxSprite(750, 160).makeGraphic(FlxG.width - 780, 540, FlxColor.BLACK);
 		bg.alpha = 0.25;
 		add(bg);
-		
-		var text:Alphabet = new Alphabet(50, 86, 'CTRL', false);
+
+		var sigh:String;
+		var sighPosX:Int;
+
+		if (controls.mobileC) {
+			sigh = "PRESS";
+			sighPosX = 44;
+		} else {
+			sigh = "CTRL";
+			sighPosX = 50;
+		}
+
+		var text:Alphabet = new Alphabet(sighPosX, 86, sigh, false);
 		text.alignment = CENTERED;
 		text.setScale(0.4);
 		add(text);
@@ -146,7 +159,16 @@ class NotesSubState extends MusicBeatSubstate
 
 		var tipX = 20;
 		var tipY = 660;
-		var tip:FlxText = new FlxText(tipX, tipY, 0, "Press RELOAD to Reset the selected Note Part.", 16);
+		var tipText:String;
+
+		if (controls.mobileC) {
+			tipText = "Press C to Reset the selected Note Part.";
+			tipY = 0;
+		} else {
+			tipText = "Press RELOAD to Reset the selected Note Part.";
+		}
+
+		var tip:FlxText = new FlxText(tipX, tipY, 0, tipText, 16);
 		tip.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		tip.borderSize = 2;
 		add(tip);
@@ -166,11 +188,20 @@ class NotesSubState extends MusicBeatSubstate
 		FlxG.mouse.visible = !controls.controllerMode;
 		controllerPointer.visible = controls.controllerMode;
 		_lastControllerMode = controls.controllerMode;
+
+        addVirtualPad(NONE, B_C);
+                virtualPad.buttonB.x = FlxG.width - 132;
+		virtualPad.buttonC.x = 0;
+		virtualPad.buttonC.y = FlxG.height - 135;
 	}
 
 	function updateTip()
 	{
+		if (controls.mobileC) {
+		// do sex
+		} else {
 		tipTxt.text = 'Hold ' + (!controls.controllerMode ? 'Shift' : 'Left Shoulder Button') + ' + Press RESET key to fully reset the selected Note.';
+                }
 	}
 
 	var _storedColor:FlxColor;
@@ -185,6 +216,8 @@ class NotesSubState extends MusicBeatSubstate
 		if (controls.BACK) {
 			FlxG.mouse.visible = false;
 			FlxG.sound.play(Paths.sound('cancelMenu'));
+			ClientPrefs.saveSettings();
+                        controls.isInSubstate = false;
 			close();
 			return;
 		}
@@ -418,6 +451,7 @@ class NotesSubState extends MusicBeatSubstate
 			else if(pointerY() >= hexTypeLine.y && pointerY() < hexTypeLine.y + hexTypeLine.height &&
 					Math.abs(pointerX() - 1000) <= 84)
 			{
+				FlxG.stage.window.textInputEnabled = true;
 				hexTypeNum = 0;
 				for (letter in alphabetHex.letters)
 				{
@@ -454,7 +488,7 @@ class NotesSubState extends MusicBeatSubstate
 				}
 				else if (holdingOnObj == colorWheel)
 				{
-					var center:FlxPoint = new FlxPoint(colorWheel.x + colorWheel.width/2, colorWheel.y + colorWheel.height/2);
+					var center:FlxPoint = FlxPoint.weak(colorWheel.x + colorWheel.width/2, colorWheel.y + colorWheel.height/2);
 					var mouse:FlxPoint = pointerFlxPoint();
 					var hue:Float = FlxMath.wrap(FlxMath.wrap(Std.int(mouse.degreesTo(center)), 0, 360) - 90, 0, 360);
 					var sat:Float = FlxMath.bound(mouse.dist(center) / colorWheel.width*2, 0, 1);
@@ -465,7 +499,7 @@ class NotesSubState extends MusicBeatSubstate
 				}
 			} 
 		}
-		else if(controls.RESET && hexTypeNum < 0)
+		else if(virtualPad.buttonC.justPressed || controls.RESET && hexTypeNum < 0)
 		{
 			if(FlxG.keys.pressed.SHIFT || FlxG.gamepads.anyJustPressed(LEFT_SHOULDER))
 			{
@@ -631,8 +665,8 @@ class NotesSubState extends MusicBeatSubstate
 		{
 			Note.initializeGlobalRGBShader(i);
 			var newNote:StrumNote = new StrumNote(150 + (480 / dataArray.length * i), 200, i, 0);
-			newNote.useRGBShader = true;
 			newNote.setGraphicSize(102);
+                        newNote.useRGBShader = true;
 			newNote.updateHitbox();
 			newNote.ID = i;
 			myNotes.add(newNote);
