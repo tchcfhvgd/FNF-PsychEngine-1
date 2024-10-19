039f1f2e4eacefd993a96e232f4dcceb5ac3c4d8
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-19 17:52:13 +0800

    Update Difficulty.hx

--- source/backend/Difficulty.hx
+++ source/backend/Difficulty.hx
@@ -5,10 +5,11 @@ class Difficulty
 	public static var defaultList(default, never):Array<String> = [
 		'Easy',
 		'Normal',
-		'Hard'
+		'Hard',
+		'erect'
 	];
 	public static var list:Array<String> = [];
-	private static var defaultDifficulty(default, never):String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
+	private static var defaultDifficulty(default, never):String = 'erect'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
 	inline public static function getFilePath(num:Null<Int> = null)
 	{
