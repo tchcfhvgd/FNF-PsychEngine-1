85a3700adc457698dd626e1c599c92206740a60c
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-19 19:56:44 +0800

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -203,7 +203,8 @@ class MainMenuState extends MusicBeatState
 						switch (optionShit[curSelected])
 						{
 							case 'story_mode':
-							        var diffic = Difficulty.getFilePath(3);
+							        Difficulty.resetList();
+								var diffic = Difficulty.getFilePath(3);
 		                                                if(diffic == null) diffic = '';
 								PlayState.storyDifficulty = 3;
 								PlayState.storyPlaylist = ['lo-fight', 'overhead', 'ballistic'];
